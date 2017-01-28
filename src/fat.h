924b9c4f44d9802a4749f4841bd11263117fa86a
Cajova-Houba <tkmushroomer@gmail.com>
2017-01-28 19:28:34 +0100

    Stuff to do.

--- src/fat.h
+++ src/fat.h
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 #include "errors.h"
 
@@ -16,6 +17,16 @@
 #define USABLE_CLUSTER_LEN      4
 #define SIGNATURE_LEN           9
 
+typedef enum {
+    FAT_UNUSED = INT32_MAX - 1,
+    FAT_FILE_END = INT32_MAX - 2,
+    FAT_BAD_CLUSTERS = INT32_MAX -3,
+    FAT_DIRECTORY = INT32_MAX -4
+} Fat_special;
+
+/*
+ * Definition of boot record.
+ */
 typedef struct{
     char volume_descriptor[DESCRIPTION_LEN];    // FS description,                  250B
     int8_t fat_type;                            // FAT type (FAT12, FAT16...),      1B
@@ -24,6 +35,16 @@ typedef struct{
     int32_t usable_cluster_count;               // max number of cluster for data   4B
     char signature[SIGNATURE_LEN];              // orion login                      9B
 } Boot_record;//                                                                    267B
+
+/*
+ * Definition of directory record.
+ */
+typedef struct {
+    char name[13];                              // File name, 8+3+'\0'
+    bool isFile;                                // Is file flag
+    int32_t size;                               // Size of the file, 0 for directory
+    int32_t start_cluster;                      // Start cluster
+} Directory;
 
 /*
  * Prints boot record.
@@ -41,5 +62,12 @@ void print_boot_record(Boot_record* boot_record);
  */
 int load_boot_record(FILE* file, Boot_record* boot_record);
 
+/*
+ * Loads a fat table from file. File table is expected to contain
+ * boot_record->usable_cluster_count records.
+ */
+
+// todo: implement
+int load_fat_table(FILE* file, Boot_record* boot_record, int32_t dest);
 
 #endif //SEMESTRALKA_FAT_H
